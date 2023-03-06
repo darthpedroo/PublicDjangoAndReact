9fa424085e5a7822c42bbcd416874ab938f54443
darthpedroo <pedrovillarino06@gmail.com>
2023-03-06 20:43:52 -0300

    Added the react app and fixed the CORS Problem

--- mynotes/api/urls.py
+++ mynotes/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getRoutes, name ="routes")
+    path('',views.getRoutes, name ="routes"),
+    path('notes/',views.getNotes, name="notes"),
+    path('notes/<str:pk>/',views.getNote, name="note"),
+
 
 ]
--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -38,16 +38,25 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
+
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'mynotes.urls'
@@ -122,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",]
